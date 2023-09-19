734f1aa9cec30da26ff09a20e56ab4ac05b87877
Aaryan Bajaj <aaryanbajaj385@gmail.com>
2023-09-19 08:53:53 +0530

    added explanation

--- BST/bstintro.cpp
+++ BST/bstintro.cpp
@@ -158,3 +158,24 @@ int main()
     
     return 0;
 }
+
+
+// This code includes several functions related to binary search trees (BST) and tree traversals. Let's go through each function and understand what it does.
+
+// 1. `levelOrderTraversal(Node *root)`: This function performs a level order traversal of a binary tree. It uses a queue to keep track of the nodes at each level. The function starts by pushing the root node into the queue. Then, it enters a while loop that continues until the queue is empty. Inside the loop, it dequeues a node from the front of the queue and checks if it is NULL. If it is NULL, it means that all nodes at the current level have been processed, so a new line is printed. If the dequeued node is not NULL, its data is printed, and its left and right children (if any) are pushed into the queue. Finally, if the queue is not empty, a NULL node is pushed into the queue to mark the end of the current level.
+
+// 2. `inorder(Node *root)`: This function performs an inorder traversal of a binary tree. It recursively visits the left subtree, prints the data of the current node, and then recursively visits the right subtree. The base case for the recursion is when the root node is NULL.
+
+// 3. `preOrder(Node *root)`: This function performs a preorder traversal of a binary tree. It prints the data of the current node, recursively visits the left subtree, and then recursively visits the right subtree. The base case for the recursion is when the root node is NULL.
+
+// 4. `postOrder(Node *root)`: This function performs a postorder traversal of a binary tree. It recursively visits the left subtree, recursively visits the right subtree, and then prints the data of the current node. The base case for the recursion is when the root node is NULL.
+
+// 5. `insertIntoBST(Node *&root, int data)`: This function inserts a new node with the given data into a binary search tree. It takes a reference to the root node as a parameter. If the root node is NULL, it creates a new node with the given data and assigns it to the root. Otherwise, it compares the data with the data of the current node. If the data is less than or equal to the current node's data, it recursively calls the function on the left subtree. Otherwise, it recursively calls the function on the right subtree. Finally, it returns the root node.
+
+// 6. `takeInput(Node *&root)`: This function takes input from the user and inserts the data into a binary search tree. It takes a reference to the root node as a parameter. It repeatedly reads data from the user until -1 is entered. For each data, it calls the `insertIntoBST` function to insert it into the tree.
+
+// 7. `minVal(Node* root)`: This function finds the minimum value node in a binary search tree. It takes the root node as a parameter and iteratively traverses the left subtree until it reaches the leftmost node, which has the minimum value. It returns the pointer to that node.
+
+// 8. `maxVal(Node* root)`: This function finds the maximum value node in a binary search tree. It takes the root node as a parameter and iteratively traverses the right subtree until it reaches the rightmost node, which has the maximum value. It returns the pointer to that node.
+
+// These functions provide various operations and traversals for binary search trees, allowing us to manipulate and analyze the data stored in the tree.
